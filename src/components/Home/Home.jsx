93c7d56501f18888465c786aae0084b7a31762a0
Ikbal Hossain ASif <asifikbalgfh312@gmail.com>
2024-09-04 18:37:50 +0600

    start suggestions

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner";
 import SearchBar from "../SearchBar";
+import Suggestions from "../Suggestions";
 
 const Home = () => {
   return (
     <div className="w-full h-full">
       <SearchBar/>
       <Banner/>
+      <Suggestions/>
     </div>
   );
 };
---
+++ src/components/Suggestions.jsx
@@ -0,0 +1,15 @@
+const Suggestions = () => {
+    return (
+        <div className="mt-6">
+            <div className="flex items-center justify-between">
+            <h1 className="text-white text-2xl">Suggestions</h1>
+            <h1 className="text-white text-lg pr-6">All</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Suggestions;
